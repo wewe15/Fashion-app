3bd86c7fac3a628f72ad3d87be5190f1dff05325
wewe15 <ahm.sami.271@gmail.com>
2022-07-17 18:49:06 +0200

    added apis login & signup for users

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import SignupPageView
+from .api.views import CreateUserView, CreateTokenView, ManageUserView
 
 urlpatterns = [
     path('signup/', SignupPageView.as_view(), name='signup'),
+    path('api/create/', CreateUserView.as_view(), name='create'),
+    path('api/token/', CreateTokenView.as_view(), name='token'),
+    path('api/me/', ManageUserView.as_view(), name='me'),
 ]
