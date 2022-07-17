47bc83cc716ed2a1f06971116726f2c5f4528b03
wewe15 <ahm.sami.271@gmail.com>
2022-07-17 21:21:59 +0200

    Added style for login & signup form

--- myshop/settings.py
+++ myshop/settings.py
@@ -130,7 +130,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles')) 
+STATICFILES_FINDERS = [ 
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
+
 AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+MEDIA_URL = '/media/' 
+MEDIA_ROOT = str(BASE_DIR.joinpath('media')) 
